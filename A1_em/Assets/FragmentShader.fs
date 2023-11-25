013c482243e5048cdd3fe2a097cbe05c5c888a35
Richella Li <61952637+richellali@users.noreply.github.com>
2023-11-25 12:33:01 -0500

    Delete A1_em directory

--- A1_em/Assets/FragmentShader.fs
+++
@@ -1,9 +0,0 @@
-#version 330
-
-uniform vec3 colour;
-
-out vec4 fragColor;
-
-void main() {
-	fragColor = vec4( colour, 1 );
-}
--- A1_em/Main.cpp
+++
@@ -1,9 +0,0 @@
-// Termm--Fall 2023
-
-#include "A1.hpp"
-
-int main( int argc, char **argv ) 
-{
-	CS488Window::launch( argc, argv, new A1(), 1024, 768, "F23 Assignment 1" );
-	return 0;
-}
